874520fa0fc35da70250ff3ac6c1a2b01eace6f5
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-16 22:52:28 +0200

    Solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (number1 < 0 
+        || number2 < 0 
+        || typeof number1 != `number` 
+        || typeof number2 != `number`) {
+        return `ERROR`;
+    }
+    if (number1 < number2){
+        // algo (number2-number1 + 1)/2 *(number1+number2) found here --> https://www.quora.com/What-is-the-sum-of-all-numbers-between-two-given-numbers#:~:text=Let%20the%20first%20given%20integer,%2B2%2C%E2%80%A6%2C%20l.
+        return (number2-number1 + 1)/2 *(number1+number2)
+    }
+    if (number1 > number2){
+        // to reverse the algo and make it work with large number first
+        return (number1-number2 + 1)/2 *(number1+number2)
+    }
+    
 };
 
 // Do not edit below this line
