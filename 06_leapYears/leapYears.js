e79a41e309c8aa36f41b6b49871b5656094222c6
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-18 22:06:11 +0200

    Add context to comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -19,9 +19,9 @@ if((year % 4 == 0)){
 // Do not edit below this line
 module.exports = leapYears;
 
-// If the year is evenly divisible by 4, go to step 2. Otherwise, return false.
-// If the year is evenly divisible by 100, go to step 3. Otherwise, return true.
-// If the year is evenly divisible by 400, return true. Otherwise, return false.
+// step 1 -- If the year is evenly divisible by 4, go to step 2. Otherwise, return false.
+// step 2 -- If the year is evenly divisible by 100, go to step 3. Otherwise, return true.
+// step 3 -- If the year is evenly divisible by 400, return true. Otherwise, return false.
 
 // True = The year is a leap year (it has 366 days).
 // False = The year is not a leap year (it has 365 days).
