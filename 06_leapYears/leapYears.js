51870366ca7031be22d7f93b97f1bd9c124ca6a8
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-18 22:04:13 +0200

    Solution and logic to exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+//step 1 
+if((year % 4 == 0)){
+    
+    // step 2
+    if(year % 100 == 0){
+        
+        // step 3
+        if(year % 400 == 0){
+            return true
+        }else return false
+
+    } else return true
+        
+}else return false
+}
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// If the year is evenly divisible by 4, go to step 2. Otherwise, return false.
+// If the year is evenly divisible by 100, go to step 3. Otherwise, return true.
+// If the year is evenly divisible by 400, return true. Otherwise, return false.
+
+// True = The year is a leap year (it has 366 days).
+// False = The year is not a leap year (it has 365 days).
