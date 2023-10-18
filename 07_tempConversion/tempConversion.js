ba3576f079120d39dfab4db31cfca86447bbf89c
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-18 22:22:40 +0200

    Solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+
+// .toFixed(1) to round result to 1 decimal
+ 
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * 5/9
+  if(temp == 0){
+    return +temp
+  }else return +temp.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = temp * 9/5 + 32
+  if(temp == 0){
+    return +temp
+  }else return +temp.toFixed(1) 
 };
 
 // Do not edit below this line
